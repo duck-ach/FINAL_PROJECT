cd8ff31eb052ccb84b1969b3ff4fc7a4fe21bce2
wjdgod777 <wjdgod777@naver.com>
2022-12-18 03:43:16 +0900

    1218

--- sporters/src/main/java/com/gdu/sporters/shop/service/ShopService.java
+++ sporters/src/main/java/com/gdu/sporters/shop/service/ShopService.java
@@ -10,6 +10,7 @@ import com.gdu.sporters.shop.domain.CartDTO;
 
 public interface ShopService {
 	public void getProductList(HttpServletRequest request, Model model);
-	public void addCart(CartDTO cart);
 	public Map<String, Object> getProductByNo(int prodNo);
+	public void addCart(CartDTO cart);
+	public void getCartList(HttpServletRequest request, Model model);
 }
