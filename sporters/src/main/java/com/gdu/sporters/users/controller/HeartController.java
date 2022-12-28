2aa597af4916ca45e29106e259cc37a37300a954
nayoung188 <nayoung188@github.com>
2022-12-28 16:47:22 +0900

    좋아요 조아요조아요

--- sporters/src/main/java/com/gdu/sporters/users/controller/HeartController.java
+++ sporters/src/main/java/com/gdu/sporters/users/controller/HeartController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gdu.sporters.users.service.HeartService;
@@ -27,25 +28,35 @@ public class HeartController {
 		return heartService.getHeartCheck(request);
 	}
 	
-	@GetMapping(value="/heart/getHeartCount", produces="application/json")
-	public Map<String, Object> getHeartCount(int userNo){
-		return heartService.getHeartCount(userNo);
-	}
-	
+	// 좋아요 표시
 	@GetMapping(value="/heart/markLike", produces="application/json")
 	public Map<String, Object> requiredLogin_mark(HttpServletRequest request){
 		return heartService.markLike(request);
 	}
 	
-	// 싫어요
+	// 싫어요 표시
 	@GetMapping(value="/heart/markhate", produces="application/json")
 	public Map<String, Object> requiredLogin_markhate(HttpServletRequest request){
 		return heartService.markhate(request);
 	}
 	
+	// 좋아요 갯수
+	@GetMapping(value="/heart/getHeartCount", produces="application/json")
+	public Map<String, Object> getHeartCount(int userNo){
+		return heartService.getHeartCount(userNo);
+	}
+	
+	// 싫어요 갯수
 	@GetMapping(value="/heart/getHateCount", produces="application/json")
 	public Map<String, Object> getHateCount(int userNo){
 		return heartService.getHateCount(userNo);
+	}
+	
+	// 좋ㅇㅏ요 식ㅎ어요
+	@ResponseBody
+	@GetMapping(value="/heart/likeNhate", produces="application/json")
+	public Map<String, Object> likeNhate(int userNo){
+		return heartService.likeNhate(userNo);
 	}
 
 	
